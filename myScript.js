75c1063bc1e8ff3d3143770b1ac289095736b68a
vikcygastaldi <127899427+vikcygastaldi@users.noreply.github.com>
2023-05-30 21:06:11 -0300

    javascript

---
+++ myScript.js
@@ -0,0 +1,14 @@
+const precioCurso1=400000
+const precioCurso2=350000
+const descu1=20
+const descu2=15
+
+let preciofinal1= precioCurso1*(descu1/100)
+console.log(preciofinal1)
+let preciofinal2= precioCurso2*(descu2/100)
+console.log(preciofinal2)
+let preciofinal3= precioCurso1*(descu2/100)
+console.log(preciofinal3)
+let preciofinal4= precioCurso2*(descu1/100)
+console.log(preciofinal4)
+
